aa4ca79210b7263c10fe52c7708fc70b72e2bc69
Tommaso Ferrari <140858716+TommyItemty@users.noreply.github.com>
2024-11-22 17:26:46 +0100

    Update streamlit_etf_scraper.py

--- streamlit_etf_scraper.py
+++ streamlit_etf_scraper.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
@@ -31,9 +32,9 @@ def scrape_etfs(url, status_placeholder):
     os.environ['WDM_CACHE_DIR'] = cache_dir
 
     try:
-        # Initialize WebDriver
+        # Initialize WebDriver with specific ChromeType for Chromium
         driver = webdriver.Chrome(
-            service=Service(ChromeDriverManager().install()),
+            service=Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()),
             options=chrome_options
         )
         
